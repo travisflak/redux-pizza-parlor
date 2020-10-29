7f14d26d7d2b333e70058d5ddf02021638ad5d31
Sam Mahler <mahler.sam@gmail.com>
2020-10-29 14:53:12 -0500

    get route for pizza

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,28 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import { HashRouter as Router, Route, Link, withRouter } from 'react-router-dom';
 import './App.css';
+import {connect} from 'react-redux';
+
+import SelectPizza from '../SelectPizza/SelectPizza';
 
 class App extends Component {
+
+  componentDidMount = () => {
+    this.getPizza();
+  }
+
+  getPizza = () => {
+    axios.get('/api/pizza').then((response) => {
+      console.log(response.data);
+    }).catch((error) => {
+      alert(error);
+    })
+  }
+
   render() {
     return (
+      // <Router>
       <div className="App">
         <header className="App-header">
           <h1 className="App-title">Prime Pizza</h1>
@@ -12,9 +30,13 @@ class App extends Component {
         <br/>
         <img src="images/pizza_photo.png"/>
         <p>Pizza is great.</p>
+        {/* <SelectPizza/> */}
+        {/* <Link to="/api/pizza">Next</Link> */}
+        {/* <Route path="/customer" component={CustomerInfo}/> */}
       </div>
+      // </Router>
     );
   }
 }
 
-export default App;
+export default connect()(App);
--- src/index.js
+++ src/index.js
@@ -2,5 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App/App';
+import { createStore, combineReducers } from 'redux';
+import { Provider } from 'react-redux'; 
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const reduxStore = createStore(
+    combineReducers({
+
+    })
+);
+
+ReactDOM.render(<Provider store={reduxStore}><App /></Provider>, document.getElementById('root'));
