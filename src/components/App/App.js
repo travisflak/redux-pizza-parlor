be5f3696ca54e791cfaf3b9ff735c2408b996894
RachelSmith500 <rachelsmith9721@gmail.com>
2020-10-29 16:45:48 -0500

    adding GET for Pizza

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,8 +1,28 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import axios from 'axios';
 import './App.css';
 
 class App extends Component {
+
+  componentDidMount() { // react Component method
+    this.getPizzas();
+  }
+  getPizzas = () => {
+    // just like $.ajax()
+    axios({
+      method: 'GET',
+      url: '/api/pizza'
+    }).then((response) => {
+      console.log(response);
+      this.props.dispatch({ type: 'SET_PIZZAS', payload: response.data})
+      // response.data will be the array of artists
+      this.setState({
+        pizzas: response.data,
+      });
+    });
+  }
+
   render() {
     return (
       <div className="App">
@@ -17,4 +37,9 @@ class App extends Component {
   }
 }
 
-export default App;
+const mapStateToProps = (reduxState) => {
+  return{
+    reduxState
+  }
+}
+export default connect(mapStateToProps)(App);
--- src/index.js
+++ src/index.js
@@ -3,4 +3,24 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App/App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+
+const pizzaReducer = (state = [], action) => {
+
+    if(action.type === 'SET_PIZZA') {
+        return action.payload;
+    } 
+    else if(action.type === 'SET_PIZZA'){
+        return [...state, action.payload]
+    }
+    return state;
+}
+const storeInstance = createStore(
+    combineReducers({
+        pizzaReducer,
+    }),
+    
+);
+ReactDOM.render(<Provider store={storeInstance}><App /></Provider>, document.getElementById('root'));
